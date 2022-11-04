a9a37295581333b12c6fcbd62b4218d7c3b567ef
MehrCodeLand <mehrcodeland@gmail.com>
2022-11-04 23:35:21 +0330

    Start Create New Main

---
+++ UniverseV2/Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UniverseV2.Areas.Admin.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
