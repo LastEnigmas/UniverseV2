077cb937a6c1a2d76c0c5d89608812fff903be69
MehrCodeLand <mehrcodeland@gmail.com>
2022-11-04 23:23:57 +0330

    Add project files.

---
+++ Data/MyDbCon/MyDb.cs
@@ -0,0 +1,26 @@
+﻿using Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.MyDbCon
+{
+    public class MyDb : DbContext
+    {
+        public MyDb(DbContextOptions<MyDb> options ) : base( options )
+        {
+
+        }
+
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Star> Stars { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+    }
+}
---
+++ UniverseV2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UniverseV2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
